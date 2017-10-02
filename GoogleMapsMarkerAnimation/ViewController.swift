ad93c98ebfe12f52a0727b0ca8416ca3af8c22a1
Viraj Padte <virajpadte@gmail.com>
2017-10-02 10:07:07 -0600

    Initial Commit

---
+++ GoogleMapsMarkerAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMapsMarkerAnimation
+//
+//  Created by Viraj on 10/2/17.
+//  Copyright © 2017 Viraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
