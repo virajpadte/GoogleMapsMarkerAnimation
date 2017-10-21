f2735363f4c1a6e0e879fd403b5947e197b4df81
Viraj Padte <virajpadte@gmail.com>
2017-10-21 03:55:41 -0600

    calculated heading from the coordinate information and animated car marker rotation for single marker. The data source for coordinate is changed from local json to a local csv file.

--- GoogleMapsMarkerAnimation/ViewController.swift
+++ GoogleMapsMarkerAnimation/ViewController.swift
@@ -22,8 +22,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //read data from the json file
-        readDataFromJsonToCoordinateArray()
+        //read data from the csv file
+        coordinateArr = csvToArrayOfCoordinates() as NSArray
+        
         initializeMap()
         addMarker()
         print("Global markers: ", gdriverMarkers)
@@ -45,46 +46,86 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func readDataFromJsonToCoordinateArray(){
-        do {
-            if let file = Bundle.main.url(forResource: "coordinates", withExtension: "json") {
-                let data = try Data(contentsOf: file)
-                let json = try JSONSerialization.jsonObject(with: data, options: [])
-                if let object = json as? [String: Any] {
-                    // json is a dictionary
-                    print(object)
-                } else if let object = json as? [Any] {
-                    // json is an array
-                    coordinateArr = NSArray(array: object)
-                } else {
-                    print("JSON is invalid")
+    
+    func csvToArrayOfCoordinates() -> [Dictionary<String, Any>] {
+        var arrayOfDictCoordinateData = [Dictionary<String,AnyObject>]()
+        var DictCoordinateData = Dictionary<String,AnyObject>()
+        
+        if let filePath = Bundle.main.path(forResource: "dataHeading", ofType: ".csv"){
+            do {
+                let contents = try String(contentsOfFile: filePath)
+                //print("contents", contents)
+                var locationDatarows  = contents.components(separatedBy: "\n").suffix(from: 2)
+                locationDatarows.removeLast()
+                for locationData in locationDatarows{
+                    let locationDataComponents = locationData.components(separatedBy: ",")
+                    DictCoordinateData["lat"] = Float(locationDataComponents[1]) as AnyObject
+                    DictCoordinateData["long"] = Float(locationDataComponents[2]) as AnyObject
+                    DictCoordinateData["heading"] = Float(locationDataComponents[3]) as AnyObject
+                    arrayOfDictCoordinateData.append(DictCoordinateData)
+                    
                 }
-            } else {
-                print("no file")
+            } catch {
+                print("File Read Error for file")
             }
-        } catch {
-            print(error.localizedDescription)
         }
+        else{
+            print("Couldn't find file path")
+        }
+        print(arrayOfDictCoordinateData)
+        return arrayOfDictCoordinateData
     }
+    
     
     func updateMarker(marker: GMSMarker, oldCoor: CLLocationCoordinate2D, newCoor: CLLocationCoordinate2D, map: GMSMapView){
         //this function is used to update the marker
         //we will be doing the animation inside a explicitly mentioned animation transaction
+        let bearing = calcBearing(oldCoordinates: oldCoor, newCoordinates: newCoor)
+        marker.groundAnchor = CGPoint(x: 0.5, y: 0.5);
+        marker.position = oldCoor; //this can be old position to make car movement to new position
         CATransaction.begin()
         CATransaction.setValue(Int(2.0), forKey: kCATransactionAnimationDuration)
         CATransaction.setCompletionBlock {
             //is executed on completion of all the transcations
-            print("Finished moving the marker now rotate")
+            marker.rotation = bearing; //found bearing value by calculation old and new Coordinates
         }
         //move marker to the new position
         marker.position = newCoor
         CATransaction.commit()
+        let updatedCamera = GMSCameraUpdate.setTarget(marker.position)
+        mapView.animate(with: updatedCamera)
         
     }
+    
+    func calcBearing(oldCoordinates: CLLocationCoordinate2D, newCoordinates: CLLocationCoordinate2D) -> Double{
+        var radians = degreeToRadians(degrees: [oldCoordinates.latitude,oldCoordinates.longitude, newCoordinates.latitude, newCoordinates.longitude]);
+        let degree = radiansToDegree(radians: atan2(sin(radians[3]-radians[1])*cos(radians[2]), cos(radians[0])*sin(radians[2])-sin(radians[0])*cos(radians[2])*cos(radians[3]-radians[1])));
+        if (degree >= 0) {
+            print("Calculated bearing degree >= 0: %0.6f",degree);
+            return degree;
+        }
+        else {
+            print("Calculated bearing degree >= 0: %0.6f",360+degree);
+            return 360+degree;
+        }
+    }
+ 
+    func degreeToRadians(degrees : [CLLocationDegrees]) -> [Double]{
+        var radians = [Double]()
+        for degree in degrees{
+            radians.append(Double.pi * degree / 180.0);
+        }
+        return radians;
+    }
+    
+    func radiansToDegree(radians: Double) -> CLLocationDegrees{
+        return radians * 180.0 / Double.pi;
+    }
+
+    
     func initializeMap(){
         //put map on the view
-        let camera = GMSCameraPosition.camera(withLatitude: 40.7416627, longitude: -74.0049708, zoom: 14)
+        let camera = GMSCameraPosition.camera(withLatitude: 40.518390000, longitude: -105.080220000, zoom: 14)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         //adding style to the map
         do {
@@ -103,7 +144,7 @@ class ViewController: UIViewController {
     func addMarker(){
         // Creates a marker in the center of the map.
         //
-        oldCoordinate = CLLocationCoordinate2DMake(40.7416627, -74.0049708)
+        oldCoordinate = CLLocationCoordinate2DMake(40.518390000, -105.080220000)
         let driverMarker = GMSMarker()
         driverMarker.position = oldCoordinate
         //need to resize the image:
